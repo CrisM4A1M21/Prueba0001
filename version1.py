79377dfffa97ef30955a7e59b1f00cf4f7f21b81
CrisM4A1M21 <cris.seyer2001@gmail.com>
2022-12-07 18:48:02 -0500

    Commit desde rama1

--- version1.py
+++ version1.py
@@ -10,6 +10,9 @@ def multiplicar(num1, num2):
 def dividir(num1, num2):
     return num1 / num2
 
+def potencia(num1, num2):
+    return num1 ** num2 
+
 
 num1 = int(input("Ingrese el valor 1: "))
 num2 = int(input("Ingrese el valor 2: "))
@@ -18,6 +21,7 @@ print("1. Sumar: ")
 print("2. Resta: ")
 print("3. Multiplicar: ")
 print("4. Dividir: ")
+print("5. Potencia: ")
 
 opcion = int(input("Elija una opcion: "))
 
@@ -29,3 +33,5 @@ if(opcion == 3):
     print(multiplicar(num1, num2))
 if(opcion == 4):
     print(dividir(num1, num2))
+if(opcion == 5):
+    print(potencia(num1, num2))
